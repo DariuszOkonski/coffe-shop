826d8aad6618e5538a4855a07a927c9f415109cc
Dariusz Okoński <darek200180@gmail.com>
2025-05-16 16:47:24 +0200

    add media queries for header

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,16 @@
 import { select } from './settings.js';
 
 const app = {
-  init: function() {
+  init: function () {
     const thisApp = this;
-      
+
     thisApp.initProduct();
   },
 
-  initProduct: function() {
+  initProduct: function () {
     console.log('initProduct');
     console.log(select);
-  }
+  },
 };
 
 app.init();
